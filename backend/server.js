14a9d0989e5a15f04acf3e9b0c73dd5217b759d6
Theanh Nguyen <nguyentheanh5088@gmail.com>
2024-11-25 04:14:44 -0800

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,31 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const conversationRoutes = require("./routes/conversation");
+const messageRoutes = require("./routes/message");
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+// allow frontend origin
+app.use(cors({origin: 'http://localhost:3000' }));
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB Atlas");
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-    // Only load routes after the database connection is established
-    const userRoutes = require("./routes/user");
-    const conversationRoutes = require("./routes/conversation");
-    const messageRoutes = require("./routes/message");
+// Routes
+app.use("/user", userRoutes);
+app.use("/conversations", conversationRoutes);
+app.use("/messages", messageRoutes);
 
-    app.use("/user", userRoutes);
-    app.use("/conversations", conversationRoutes);
-    app.use("/messages", messageRoutes);
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
