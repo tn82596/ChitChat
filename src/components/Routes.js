743b3b76c24ec50faaf092b667c4e2eda39e4974
Ashley Wu <me@ashleywu.net>
2024-11-25 01:17:06 -0800

    Restructure frontend with components and styles, create Home/Login/Signup.js, import react-router-dom

---
+++ src/components/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
