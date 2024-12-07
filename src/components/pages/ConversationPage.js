12def42cd9c20805b63518ceae4cf6b3c57b65e2
Ashley Wu <me@ashleywu.net>
2024-12-06 16:02:05 -0800

    get reactions feature working

--- src/components/pages/ConversationPage.js
+++ src/components/pages/ConversationPage.js
@@ -76,8 +76,8 @@ const ConversationPage = () => {
     if (convoID) {
       fetchMessages().then(() => {
         if (isInitialLoad.current) {
-          scrollToBottom(); 
-          isInitialLoad.current = false; 
+          scrollToBottom();
+          isInitialLoad.current = false;
         }
       });
 
@@ -170,151 +170,160 @@ const ConversationPage = () => {
         if (error.response && error.response.status === 404) {
           setSearchResults({ message: "No messages found matching the search." });
         }
-      } }catch (error) {
-        console.error('Error searching messages:', error);
-        setSearchResults([]);
-      } finally {
-        setIsSearching(false);
       }
-    };
+    } catch (error) {
+      console.error('Error searching messages:', error);
+      setSearchResults([]);
+    } finally {
+      setIsSearching(false);
+    }
+  };
 
-    const handleSignOut = () => {
-      localStorage.removeItem('token');
-      navigate('/login');
-    };
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
 
-    const scrollToBottom = () => {
-      messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-    };
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    const handleContextMenu = (e, messageID) => {
-      e.preventDefault();
-      setContextMenu({
-        x: e.clientX,
-        y: e.clientY,
-        messageID: messageID, // Store the message ID for later use
-      });
-    };
+  const handleContextMenu = (e, messageID) => {
+    e.preventDefault();
+    setContextMenu({
+      x: e.clientX,
+      y: e.clientY,
+      messageID: messageID, // Store the message ID for later use
+    });
+  };
 
-    const closeContextMenu = () => {
-      setContextMenu(null);
-    };
+  const closeContextMenu = () => {
+    setContextMenu(null);
+  };
 
-    const handleThemeChange = (event) => {
-      setTheme(event.target.value);
-    };
+  const handleThemeChange = (event) => {
+    setTheme(event.target.value);
+  };
 
-    const handleReaction = async (messageId, reaction) => {
+  const handleReaction = async (messageId, reaction) => {
+      const token = localStorage.getItem('token');
       try {
-        const token = localStorage.getItem('token');
-        await axios.post(
-          `http://localhost:5001/messages/${messageId}/reactions`,
+        const response = await axios.put(`http://localhost:5001/messages/react/${messageId}`, 
           { reaction },
-          { headers: { Authorization: `Bearer ${token}` } }
+          { headers: { Authorization: `Bearer ${token}` } } 
         );
-  
-        socket.emit("conversationUpdated", convoID);
+    
+        const updatedMessage = response.data;
+    
+        // Update the local message state
+        setMessages((prevMessages) =>
+          prevMessages.map((message) =>
+            message._id === updatedMessage._id ? updatedMessage : message
+          )
+        );
       } catch (error) {
-        console.error('Error adding reaction:', error);
+        console.error("Failed to update reaction:", error.response?.data || error.message);
       }
-    };
-  
-    const toggleReactions = (messageId) => {
-      setSelectedMessage(selectedMessage === messageId ? null : messageId);
-    };
+    
+  };
 
-    return (
-      <div className="conversation-page" onClick={closeContextMenu}>
-        <button className="sign-out-button" onClick={handleSignOut}>
-          Sign Out
+  const toggleReactions = (messageId) => {
+    setSelectedMessage(selectedMessage === messageId ? null : messageId);
+  };
+
+  return (
+    <div className="conversation-page" onClick={closeContextMenu}>
+      <button className="sign-out-button" onClick={handleSignOut}>
+        Sign Out
+      </button>
+
+      <h2 className="conversation-title">Conversation</h2>
+
+      <button className="go-back-button" onClick={() => navigate('/recent-messages')}>
+        Go Back
+      </button>
+
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search messages..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="search-button" onClick={handleSearch} disabled={isSearching}>
+          {isSearching ? 'Searching...' : 'Search'}
         </button>
+      </div>
 
-        <h2 className="conversation-title">Conversation</h2>
-
-        <button className="go-back-button" onClick={() => navigate('/recent-messages')}>
-          Go Back
-        </button>
-
-        <div className="search-container">
-          <input
-            type="text"
-            className="search-input"
-            placeholder="Search messages..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button className="search-button" onClick={handleSearch} disabled={isSearching}>
-            {isSearching ? 'Searching...' : 'Search'}
-          </button>
-        </div>
-
-        <div
+      <div
         className="messages-container"
         style={{ backgroundColor: themes[theme] }} // Dynamic theme background
       >
-          {isSearching ? (
-            <p>Searching...</p>
-          ) : searchResults.message ? (
-            <div className="search-results">
-              <h3 className="search-title">No search results</h3>
-            </div>
-          ) : searchResults.length > 0 ? (
-            <div className="search-results">
-              <h3 className="search-title">Search Results</h3>
-              {searchResults.map((message) => (
-                <div
-                  key={message._id}
-                  className="search-message-item"
-                  onClick={() => navigate(`/search/${message._id}`)} // Navigate to search page
-                  style={{ cursor: 'pointer' }}
-                >
-                  <div className="search-message-header">
-                    <span className="search-message-sender">{userNames[message.sender]}</span>
-                    <span className="search-message-timestamp">
-                      {new Date(message.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                    </span>
-                  </div>
-                  <div className="search-message-content">
-                    {message.content}
-                  </div>
-                  <hr className="search-divider" />
+        {isSearching ? (
+          <p>Searching...</p>
+        ) : searchResults.message ? (
+          <div className="search-results">
+            <h3 className="search-title">No search results</h3>
+          </div>
+        ) : searchResults.length > 0 ? (
+          <div className="search-results">
+            <h3 className="search-title">Search Results</h3>
+            {searchResults.map((message) => (
+              <div
+                key={message._id}
+                className="search-message-item"
+                onClick={() => navigate(`/search/${message._id}`)} // Navigate to search page
+                style={{ cursor: 'pointer' }}
+              >
+                <div className="search-message-header">
+                  <span className="search-message-sender">{userNames[message.sender]}</span>
+                  <span className="search-message-timestamp">
+                    {new Date(message.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                  </span>
                 </div>
-              ))}
-            </div>
-          ) : (
-            <div className="messages-list">
-              {messages.map((message) => (
-                <div
-                  key={message._id}
-                  className={`message-item ${message.sender === userID ? 'sent' : 'received'}`}
-                  onContextMenu={(e) => handleContextMenu(e, message._id)}
-                  onClick={() => toggleReactions(message._id)}
-                >
-                  <div className={`message-sender ${message.sender === userID ? 'sent-name' : 'received-name'}`}>
-                    {userNames[message.sender]}
-                  </div>
-                  <div className="message-content">
-                    {message.content}
-                  </div>
-                  <div className="message-timestamp">
-                    {new Date(message.timestamp).toLocaleString()}
-                  </div>
-
-                  {selectedMessage === message._id && (
-                    <div className="reactions-container">
-                      <button onClick={() => handleReaction(message._id, '❤️')} className="reaction-button">❤️</button>
-                      <button onClick={() => handleReaction(message._id, '👍')} className="reaction-button">👍</button>
-                      <button onClick={() => handleReaction(message._id, '👎')} className="reaction-button">👎</button>
-                    </div>
-                  )}
+                <div className="search-message-content">
+                  {message.content}
                 </div>
-              ))}
-              <div ref={messagesEndRef}></div>
-            </div>
-          )}
-        </div>
+                <hr className="search-divider" />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="messages-list">
+            {messages.map((message) => (
+              <div
+                key={message._id}
+                className={`message-item ${message.sender === userID ? 'sent' : 'received'}`}
+                onContextMenu={(e) => handleContextMenu(e, message._id)}
+                onClick={() => toggleReactions(message._id)}
+              >
+                <div className={`message-sender ${message.sender === userID ? 'sent-name' : 'received-name'}`}>
+                  {userNames[message.sender]}
+                </div>
+                {selectedMessage === message._id && (
+                  <div className="reactions-container">
+                    <button onClick={() => handleReaction(message._id, 'love')} className="reaction-button">❤️{message.reactions.love.length}</button>
+                    <button onClick={() => handleReaction(message._id, 'like')} className="reaction-button">👍{message.reactions.like.length}</button>
+                    <button onClick={() => handleReaction(message._id, 'dislike')} className="reaction-button">👎{message.reactions.dislike.length}</button>
+                  </div>
+                )}
+                <div className="message-content">
+                  {message.content}
+                </div>
+                <div className="message-timestamp">
+                  {new Date(message.timestamp).toLocaleString()}
+                </div>
 
-        {/* Theme Selector */}
+                
+              </div>
+            ))}
+            <div ref={messagesEndRef}></div>
+          </div>
+        )}
+      </div>
+
+      {/* Theme Selector */}
       <div className="theme-selector">
         <label htmlFor="theme">Select Theme:</label>
         <select id="theme" value={theme} onChange={handleThemeChange}>
@@ -324,31 +333,31 @@ const ConversationPage = () => {
         </select>
       </div>
 
-        {/* Delete Button context menu */}
-        {contextMenu && (
-          <div
-            className="delete-context-menu"
-            style={{ top: contextMenu.y, left: contextMenu.x }}
-            onClick={() => handleDeleteMessage(contextMenu.messageID)}
-          >
-            <button>Delete</button>
-          </div>
-        )}
-
-        {/* Message Input */}
-        <div className="message-input-container">
-          <textarea
-            className="message-input"
-            placeholder="Type your message..."
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-          ></textarea>
-          <button className="send-button" onClick={handleSendMessage} disabled={!newMessage.trim()}>
-            Send
-          </button>
+      {/* Delete Button context menu */}
+      {contextMenu && (
+        <div
+          className="delete-context-menu"
+          style={{ top: contextMenu.y, left: contextMenu.x }}
+          onClick={() => handleDeleteMessage(contextMenu.messageID)}
+        >
+          <button>Delete</button>
         </div>
-      </div>
-    );
-  };
+      )}
 
-  export default ConversationPage;
+      {/* Message Input */}
+      <div className="message-input-container">
+        <textarea
+          className="message-input"
+          placeholder="Type your message..."
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        ></textarea>
+        <button className="send-button" onClick={handleSendMessage} disabled={!newMessage.trim()}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ConversationPage;
